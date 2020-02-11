ce5950c361c0ddf96639adb7a4a0261f729132b6
TowaAoyagi <tab.git.kamui@gmail.com>
2020-02-11 10:23:56 +0900

    Initial commit

---
+++ app/src/main/java/app/aoyagi/makkan/profile/MainActivity.kt
@@ -0,0 +1,24 @@
+package app.aoyagi.makkan.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        profileImage.visibility = View.INVISIBLE
+        profileCommentText.visibility = View.INVISIBLE
+
+        button1.setOnClickListener {
+
+            profileImage.visibility = View.VISIBLE
+            profileCommentText.visibility = View.VISIBLE
+        }
+
+    }
+}
